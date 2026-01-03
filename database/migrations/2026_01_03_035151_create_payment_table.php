97a52c3cd81870106c668f317bb818122e4cb019
Nurin Iffah binti Ahmad Borhan <nuriniffah@gmail.com>
2026-01-03 13:07:34 +0800

    database

--- database/migrations/2026_01_03_035151_create_payment_table.php
+++ database/migrations/2026_01_03_035151_create_payment_table.php
@@ -12,7 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('payment', function (Blueprint $table) {
-            $table->id();
+            $table->id('paymentID');
+            
+            $table->varchar('paymentStatus');
+            $table->varchar('method', 50);
+            $table->dateTime('paymentDate');
+            $table->decimal('discountedPrice', 10, 2);
+            $table->decimal('amount', 10, 2);
+            $table->decimal('grandTotal', 10, 2);
+            
+            $table->foreignId('bookingID')->constrained(table: 'booking', column: 'bookingID');
+
             $table->timestamps();
         });
     }
