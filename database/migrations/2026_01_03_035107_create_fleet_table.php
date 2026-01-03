1aea7be3ddc7d170a6d337ecff723b3268ef9eaa
SYARAH AQILAH <syarahyaacob@gmail.com>
2026-01-03 13:06:59 +0800

    fleet

--- database/migrations/2026_01_03_035107_create_fleet_table.php
+++ database/migrations/2026_01_03_035107_create_fleet_table.php
@@ -12,8 +12,27 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('fleet', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id('plateNumber');
+            $table->string('modelName');
+            $table->integer('year');
+            $table->string('photos');
+            $table->string('ownerName');
+            $table->integer('ownerIc');
+            $table->longint('ownerPhone');
+            $table->string('ownerEmail');
+            $table->string('roadtaxStat');
+            $table->date('taxActivedate');
+            $table->date('taxExpirydate');
+            $table->string('insuranceStat');
+            $table->date('insuranceActivedate');
+            $table->date('insuranceExpirydate');
+            $table->string('status');
+            $table->text('note');
+        
+            $table->foreignId('matricNum')->constrained(table: 'customer', column: 'matricNum');
+            $table->foreignId('staffID')->constrained(table: 'staff', column: 'staffID');
+
+               $table->timestamps();
         });
     }
 
