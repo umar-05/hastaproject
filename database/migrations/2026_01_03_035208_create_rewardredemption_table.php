357b6bbd6ee9741a2e670bd1584d1e4ed92ba0b7
MUHAMMAD FIRDAUS BIN MD LANI <chrishyungg97@gmail.com>
2026-01-03 13:12:15 +0800

    final

--- database/migrations/2026_01_03_035208_create_rewardredemption_table.php
+++ database/migrations/2026_01_03_035208_create_rewardredemption_table.php
@@ -12,13 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('rewardredemption', function (Blueprint $table) {
-            $table->id('matricNum');
-            $table->id('rewarID');
-
-            $table->dateTime(redemptionDate)
+            $table->dateTime('redemptionDate');
 
             $table->foreignId('matricNum')->constrained(table: 'customer', column: 'matricNum');
             $table->foreignId('rewardID')->constrained(table: 'reward', column: 'rewardID');
+
+            $table->primary(['matricNum', 'rewardID']);
 
             $table->timestamps();
         });
