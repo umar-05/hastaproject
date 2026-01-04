08c177bcdef5f92882662edca0a4945462efb993
Muhammad Umar Rusyad <umarrusyad@gmail.com>
2026-01-05 00:15:23 +0800

    Update staff.blade.php

--- resources/views/layouts/staff.blade.php
+++ resources/views/layouts/staff.blade.php
@@ -151,6 +151,7 @@
             </header>
 
             <main class="flex-1 overflow-y-auto bg-gray-50/50 p-8">
+                {{ $slot }}
                 @yield('content')
             </main>
         </div>
