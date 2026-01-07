4ff896b0ae246fd1a8fe1635f92681361a1b5e5e
Nurin Iffah binti Ahmad Borhan <nuriniffah@gmail.com>
2026-01-07 23:43:36 +0800

    Update customermanagement.blade.php

--- resources/views/staff/customermanagement.blade.php
+++ resources/views/staff/customermanagement.blade.php
@@ -4,7 +4,7 @@
             
             {{-- Header Section --}}
             <div class="mb-8">
-                <h1 class="text-3xl font-medium text-gray-900 tracking-tight">Customer management</h1>
+                <h1 class="text-3xl font-medium text-gray-900 tracking-tight">Customer Management</h1>
                 <p class="text-base text-gray-500 mt-1">Manage customer information, records, and account statuses</p>
             </div>
 
