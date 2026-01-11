610808aba6cb0f26b3b68571572881e13a4fdd80
MUHAMMAD FIRDAUS BIN MD LANI <chrishyungg97@gmail.com>
2026-01-11 22:04:40 +0800

    deleted delete account partial

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -47,11 +47,6 @@
                     </div>
                 </div>
 
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg border border-gray-100">
-                    <div class="max-w-xl">
-                        @include('profile.partials.delete-user-form')
-                    </div>
-                </div>
             </div>
 
             <div x-show="activeTab === 'documents'"
