80906a7e69bd2d9859b693d78f2e9c753bafce70
Muhammad Umar Rusyad <umarrusyad@gmail.com>
2026-01-31 01:29:30 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+        }
         // Now this line will work correctly
         Blade::component('layouts.staff', 'layouts.staff');
     }
